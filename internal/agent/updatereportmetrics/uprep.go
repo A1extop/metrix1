0736f8c07ec7c3e850aaaab9496979ac650428e9
A1extop <blednykh1995@inbox.ru>
2024-09-25 00:15:48 +0300

    чутка iter15

--- internal/agent/updatereportmetrics/uprep.go
+++ internal/agent/updatereportmetrics/uprep.go
@@ -2,6 +2,7 @@ package updatereportmetrics
 
 import (
 	"context"
+
 	"net/http"
 	"time"
 
@@ -19,10 +20,9 @@ type Updater struct {
 
 func (u *Updater) Action(ctx context.Context, parameters *config.Parameters) {
 	pollTicker := time.NewTicker(time.Duration(parameters.PollInterval) * time.Second)
-	reportTicker := time.NewTicker(time.Duration(parameters.ReportInterval) * time.Second)
-	client := &http.Client{}
-	semaphore := make(chan struct{}, parameters.RateLimit)
+
 	go func() {
+		defer pollTicker.Stop()
 		for {
 			select {
 			case <-ctx.Done():
@@ -34,17 +34,35 @@ func (u *Updater) Action(ctx context.Context, parameters *config.Parameters) {
 	}()
 
 	go func() {
+		workerPool(u, ctx, parameters)
+	}()
+}
+
+func workerPool(u *Updater, ctx context.Context, parameters *config.Parameters) {
+	reportTicker := time.NewTicker(time.Duration(parameters.ReportInterval) * time.Second)
+	client := &http.Client{}
+	reportChan := make(chan struct{}, parameters.RateLimit)
+
+	for i := 0; i < parameters.RateLimit; i++ {
+		go func() {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case <-reportChan:
+					u.updater.ReportMetrics(client, "http://"+parameters.AddressHTTP, parameters.Key)
+				}
+			}
+		}()
+	}
+
+	go func() {
 		for {
 			select {
 			case <-ctx.Done():
 				return
 			case <-reportTicker.C:
-				semaphore <- struct{}{}
-				go func() {
-					defer func() { <-semaphore }()
-
-					u.updater.ReportMetrics(client, "http://"+parameters.AddressHTTP, parameters.Key)
-				}()
+				reportChan <- struct{}{}
 			}
 		}
 	}()
